a08e400b30cd8200919233f9bcb6227b18a8933e
Loshana Aloka <acursed_comrade@proton.me>
2024-02-22 01:40:57 +0530

    TODOs

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # sandbox-research
+
 This repository contains my study on Windows internals and my research on sandbox concepts with proof-of-concepts dedicated to Windows.
 
 ## Resources
-- [eWDK](https://learn.microsoft.com/en-us/windows-hardware/drivers/develop/using-the-enterprise-wdk#getting-started)
+
+- [Hardware developer documentation](https://learn.microsoft.com/en-us/windows-hardware/drivers/)
+- [eWDK](https://learn.microsoft.com/en-us/windows-hardware/drivers/develop/using-the-enterprise-wdk)
 - [WDK bindings for Rust](https://github.com/microsoft/windows-drivers-rs/)
--- sandboxdrv/src/driver.rs
+++ sandboxdrv/src/driver.rs
@@ -1,8 +1,11 @@
 use wdk::println;
 use wdk_sys::{DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, STATUS_SUCCESS};
 
+// TODO: refer to documentation for defining correct details and capabilities
+// in INX file. Some work left to be done make a proper DriverEntry and DriverExit routines.
+
 #[export_name = "DriverEntry"]
-pub unsafe extern "C" fn driver_entry(
+unsafe extern "system" fn driver_entry(
     driver: &mut DRIVER_OBJECT,
     _device_path: PCUNICODE_STRING,
 ) -> NTSTATUS {
@@ -13,6 +16,6 @@ pub unsafe extern "C" fn driver_entry(
     STATUS_SUCCESS
 }
 
-pub unsafe extern "C" fn driver_exit(_driver: *mut DRIVER_OBJECT) {
+unsafe extern "C" fn driver_exit(_driver: *mut DRIVER_OBJECT) {
     println!("sandboxdrv.sys: Buh bye!");
 }
